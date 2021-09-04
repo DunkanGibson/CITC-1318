ebbfd2c77f05b50ae463546b660da5dc5724e615
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-09-04 09:44:59 -0400

    First assignment for the class

---
+++ Code/Chapter1.java
@@ -0,0 +1,14 @@
+package com.CITC1318.course.Chapters;
+
+public class Chapter1 {
+
+    public Chapter1() {
+
+        System.out.println("Hello from Chapter1!");
+
+    }
+
+    public String test() {
+        return "test";
+    }
+}
