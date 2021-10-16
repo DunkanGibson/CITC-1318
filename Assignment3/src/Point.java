a28e0212cc647ceb583f9abecfd604c935870beb
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-10-16 10:49:13 -0400

    Creation of classes and done with point class

---
+++ Assignment3/src/Point.java
@@ -0,0 +1,28 @@
+public class Point {
+
+    private int width = 0;
+    private int height = 0;
+
+    public Point(int theWidth, int theHeight){
+        width = theWidth;
+        height = theHeight;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int theWidth) {
+        this.width = theWidth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int theHeight) {
+        this.height = theHeight;
+    }
+
+    
+}
