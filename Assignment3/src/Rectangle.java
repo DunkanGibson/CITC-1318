2d9f850b381c56206836d588180d4538279e02ae
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-10-16 12:21:38 -0400

    Had issue with VScode

--- Assignment3/src/Rectangle.java
+++ Assignment3/src/Rectangle.java
@@ -1,3 +1,27 @@
-public class Rectangle {
-    
+public class Rectangle implements FigureGeometry{
+    private Point point;
+
+    public Rectangle(Point p){
+        point = p;
+    }
+
+    public int getWidth() {
+        return point.getWidth();
+    }
+
+    public int getHeight(){//returns the height of a Rectangle object as follows:
+        return point.getHeight();
+    }
+
+    public float getArea(){//returns the area of a Rectangle object as follows:
+        return getWidth() * getHeight ();
+    }
+
+    public float getPerimeter(){//returns the perimeter of a Rectangle object as follows:
+        return (getWidth() + getHeight()) * 2;
+    }
+
+    public void setPoint(Point p){//assigns the point of a Rectangle object as follows:
+        point = p;
+    }
 }
